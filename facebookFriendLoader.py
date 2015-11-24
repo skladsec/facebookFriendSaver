840ed217942fab3715d20b15c7bf55378eb7b936
root <root@localhost.localdomain>
2015-11-24 14:57:58 -0200

    loader working

--- facebookFriendLoader.py
+++ facebookFriendLoader.py
@@ -12,13 +12,11 @@ from splinter.request_handler.status_code import HttpResponseError
 
 def main(argv):
 	email = None
-	txtopt = None
-	profile = None
-	self = None
+	txtipt = None
 	socks = None
 	socksPort = None
 	try:
-		opts, args = getopt.getopt(argv, "ho:m:p:s:S:P:",["port=","socks=","self=","profile=","output=","mail=","help"])
+		opts, args = getopt.getopt(argv, "hi:m:S:P:",["port=","socks=","input=","mail=","help"])
 	except:
 		print "Use --help for help"
 		sys.exit(2)
@@ -28,25 +26,19 @@ def main(argv):
 			print 'Usage %s options \n' % (os.path.basename(__file__))
 			print '      -h, --help           This help'
 			print '      -m, --mail           Your facebook login email'
-			print '      -o, --output         Your output file name'
-			print '      -p, --profile        Profile to capture friends(name after facebook.com/)'
-			print '      -s, --self           Your profile name(name after facebook.com/)'
+			print '      -i, --input          Your input file name'
 			print '      -S, --socks          Socks Proxy Address for Tor use'
 			print '      -P, --port           Port Socks for Tor use'
 			sys.exit()
-		elif opt in ("-o","--output"):
-			txtopt = arg
+		elif opt in ("-i","--input"):
+			txtipt = arg
 		elif opt in ("-m","--mail"):
 			email = arg
-		elif opt in ("-p","--profile"):
-			profile = arg
-		elif opt in ("-s","--self"):
-			self = arg
 		elif opt in ("-S","--socks"):
 			socks = arg
 		elif opt in ("-P","--port"):
 			socksPort = arg
-	if not email or not txtopt or not self:
+	if not email or not txtipt:
 		print 'Use --help for help'
 		sys.exit()
 
@@ -73,33 +65,16 @@ def main(argv):
 			sys.exit()
 		
 		try:
-			fileopt = open(txtopt, 'a')
+			fileipt = open(txtipt, 'r')
 		except:
-			sys.exit('Unable to open file %s' % txtopt)
+			sys.exit('Unable to open file %s' % txtipt)
 
-		if not profile:
-			browser.find_link_by_text("Profile").click()
-			print 'Accessing profile at %s\n' % browser.url
-			browser.find_link_by_text("Friends").click()
-			print 'Accessing friends at %s\n' % browser.url
-		else:
-			url = 'https://m.facebook.com/%s/friends?refid=17' % profile
-			print 'Accessing profile friends at %s\n' % url
-			browser.visit(url)
-		friends = browser.find_by_css('a')
-		notList = ["/a/mobile/friends/add_friend.php","language.php","/help/","/settings/","/pages/","/bugnub/","/policies/","/logout","/home","/friends","/messages/","/notifications.php","/buddylist.php","/menu/","/photo.php","/mbasic/","%s"%profile,"%s"%self]
-		for friend in friends:
-			if all([x not in friend['href'] for x in notList ]):
-				fileopt.write('%s\n' % friend['href'])
-				print '%s' % friend.value
+		for line in fileipt:
+			browser.visit(line)
+			addButton = browser.find_link_by_text('Add Friend')
+			if len(addButton) > 0:
+				addButton[0].click()
 
-		while browser.is_element_present_by_css("#m_more_friends"):
-			browser.find_by_css('#m_more_friends a').first.click()		
-			friends = browser.find_by_css('a')
-			for friend in friends:
-				if all([x not in friend['href'] for x in notList ]):
-					fileopt.write('%s\n' % friend['href'])
-					print '%s' % friend.value
 
 if __name__ == "__main__":
 	try:
